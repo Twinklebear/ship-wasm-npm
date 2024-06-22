63ff0de5568e1e225736b84223b22083d0ac7503
Will Usher <will@willusher.io>
2024-06-22 09:18:01 -0700

    let the exception go up to caller

--- npm_package/src/index.ts
+++ npm_package/src/index.ts
@@ -2,9 +2,5 @@ import CppApp from "./cpp/app.js";
 
 export async function runWasm(canvas: string) {
   const app = await CppApp();
-  try {
-    app.callMain([canvas]);
-  } catch (e) {
-    console.error(e);
-  }
+  app.callMain([canvas]);
 }
